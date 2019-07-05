05a24475f9fa53ad4e1fd368be774116c4b4a620
Andrzej Zaborski <andrzejzab@gmail.com>
2019-07-05 17:02:44 +0200

    Element added

---
+++ src/components/EventInfoCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export class EventInfoCard extends Component {
+    render() {
+        return (
+            <div className="eventInfoCard">
+                <h1 className="eventInfoCardHeader">Event Name</h1>
+            </div>
+        )
+    }
+}
