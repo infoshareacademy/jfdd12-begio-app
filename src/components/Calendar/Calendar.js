8c2afc9434941dc1876482536fec8e5fe8671383
TekMal <k.kochanowska6@gmail.com>
2019-07-09 20:43:48 +0200

    changes in file events.json - add new h for salsa, change buttons and position of calendar'

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -11,11 +11,11 @@ function EventsCalendar(props){
   const userEvents = props.events.map(event => 
     Object.assign({}, {
    'title': event.title,
-   'start': new Date(event.startDate.year, event.startDate.month - 1 , event.startDate.day, event.startDate.time[0], event.startDate.time[0]),
-   'end': new Date(event.endDate.year, event.endDate.month - 1, event.endDate.day, event.endDate.time[0], event.endDate.time[0])
+   'start': new Date(event.startDate.year, event.startDate.month - 1 , event.startDate.day, event.startDate.time[0], event.startDate.time[1]),
+   'end': new Date(event.endDate.year, event.endDate.month - 1, event.endDate.day, event.endDate.time[0], event.endDate.time[1])
  }))
   console.log(props.events)
-  return  <div style={{ height: 600, width:1000}}>
+  return  <div style={{marginLeft:20, marginTop: 50, height: 600, width:1000}}>
   <Calendar
     localizer={localizer}
     events={userEvents}
@@ -24,7 +24,7 @@ function EventsCalendar(props){
       day: true
     }}
     eventPropGetter={() => ({style: {backgroundColor: 'green'}})}
-    messages={{next:"Następny",previous:"Poprzedni",today:"Dziś", month: "Miesiąc", day: "Dziś"}}
+    messages={{next:">>",previous:"<<",today:"Dziś", month: "Miesiąc", day: "Dzień"}}
   />
 </div>
 }
