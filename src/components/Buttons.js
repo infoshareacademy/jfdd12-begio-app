127ab018105c4ad999779df7ab12a701a489f0d5
Ania Jelinska <ania.jelin@wp.pl>
2019-07-10 15:38:54 +0200

    added Function onClose to ExitButton

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import styles from "./EventView.module.css";
 
-export const ExitButton = () => {
-    return (
-        <Button className={styles.exitButton}>X</Button>
-    )
-}
+export const ExitButton = ({ onClick }) => {
+  return (
+    <Button onClick={onClick} className={styles.exitButton}>
+      X
+    </Button>
+  );
+};
 
 export const ButtonGo = () => {
-    return (
-        <button className={styles.buttonGo}>GO!</button>
-    )
-}
+  return <button className={styles.buttonGo}>GO!</button>;
+};
--- src/components/EventView.js
+++ src/components/EventView.js
@@ -9,34 +9,35 @@ import { ButtonGo } from "./Buttons";
 import calendar from "./calendar.png";
 import styles from "./EventView.module.css";
 
-function EventView({ event }) {
+function EventView({ event, onClose }) {
   return (
     <div className={styles.EventView}>
       <div className={styles.topView}>
         <EventName name={event.title} />
         <ButtonGo />
-        <ExitButton />
+        <ExitButton onClick={onClose} />
       </div>
       <AutoPlayCarousel images={event.images} />
       <div className={styles.bottomView}>
-      <div className={styles.eventTime}>
-        <img src={calendar} className={styles.calendar} alt="calendar" />
-        <EventDate
-          startYear={event.startDate.year}
-          startMonth={event.startDate.month}
-          startDay={event.startDate.day}
-          startTime={event.startDate.time}
-          endYear={event.endDate.year}
-          endMonth={event.endDate.month}
-          endDay={event.endDate.day}
-          endTime={event.endDate.time}
-        />
+        <div className={styles.eventTime}>
+          <img src={calendar} className={styles.calendar} alt="calendar" />
+          <EventDate
+            startYear={event.startDate.year}
+            startMonth={event.startDate.month}
+            startDay={event.startDate.day}
+            startTime={event.startDate.time}
+            endYear={event.endDate.year}
+            endMonth={event.endDate.month}
+            endDay={event.endDate.day}
+            endTime={event.endDate.time}
+          />
           <EventPlace
-          city={event.address.city}
-          street={event.address.street}
-          house={event.address.houseNumber}
-        /></div>
-        
+            city={event.address.city}
+            street={event.address.street}
+            house={event.address.houseNumber}
+          />
+        </div>
+
         <EventDescription description={event.description} />
       </div>
     </div>
