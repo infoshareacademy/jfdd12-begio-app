64e2b80134abd000661dee6cf94eb65d695caa86
Kamila Kochanowska <k.kochanowska6@gmail.com>
2019-07-12 16:05:31 +0200

    wip calendar

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,15 +4,19 @@ import { EventList } from "../EventList"
 
 function UserProfile(props){
     console.log(props.myFuckingEvents)
-return <div>
-    <EventList  myEvents={props.myEvents}
-                setFavourite={props.setMyEvents}
-                events={props.myFuckingEvents}
-                addMyEvent={props.addMyEvent}
-                removeMyEvent={props.removeMyEvent}
-            />
-    <EventsCalendar events = {props.events} userEvents = {props.myEvents}/>
-    </div>
+return <div>{props.myFuckingEvents === [] ? <div> <EventList  myEvents={props.myEvents}
+setFavourite={props.setMyEvents}
+events={props.myFuckingEvents}
+addMyEvent={props.addMyEvent}
+removeMyEvent={props.removeMyEvent}
+/>
+<EventsCalendar events = {props.events} userEvents = {props.myEvents}/> 
+</div>
+: 
+<EventsCalendar events = {props.events} userEvents = {props.myEvents}/> 
+}
+
+</div>  
 }
 
 export default UserProfile
