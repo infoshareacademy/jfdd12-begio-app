440980e2f0a94745a45fd5194b83499180b3a705
Lukasz Patalan <lukasz.patalan@gmail.com>
2019-07-12 17:30:07 +0200

    PROBLEMS

--- src/App.js
+++ src/App.js
@@ -1,87 +1,91 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect
-} from "react-router-dom"
-import EventsCalendar from "./components/Calendar"
-import { EventList } from "./components/EventList"
-import { MapView } from "./components/MapView"
-import events from "./events.json"
-import { Navbar } from "./components/Navbar"
-import users from "./users.json"
-import UserProfile from "./components/UserProfile"
-import "./App.css"
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import EventsCalendar from "./components/Calendar";
+import { EventList } from "./components/EventList";
+import { MapView } from "./components/MapView";
+import events from "./events.json";
+import { Navbar } from "./components/Navbar";
+import users from "./users.json";
+import UserProfile from "./components/UserProfile";
+import "./App.css";
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(users[0])
-    const [myEvents, setMyEvents] = useState([])
+  const [currentUser, setCurrentUser] = useState(users[0]);
+  const [myEvents, setMyEvents] = useState([]);
 
-    useEffect(() => {
-        console.log(myEvents)
-    }, [myEvents])
-    const addMyEvent = id => {
-        setMyEvents([...myEvents, id])
-    }
-    const removeMyEvent = id => {
-        const eventToDelete = myEvents.find(eventId => eventId === id)
-        const eventsWithDeletedEvent = myEvents.filter(
-            eventId => eventId !== eventToDelete
-        )
-        setMyEvents([...eventsWithDeletedEvent])
-    }
-    return (
-        <Router>
-            <div>
-                <Navbar user={currentUser} />
-                <Switch>
-                    <Route
-                        exact
-                        path="/"
-                        render={() => (
-                            <div className="appView">
-                                <MapView />
-                                <EventList
-                                    myEvents={myEvents}
-                                    setFavourite={setMyEvents}
-                                    events={events}
-                                    addMyEvent={addMyEvent}
-                                    removeMyEvent={removeMyEvent}
-                                />
-                            </div>
-                        )}
-                    />
-                    <Route path="/callender" component={App} />
-                    <Route
-                        path="/userProfile"
-                        render={() => (
-                            <UserProfile
-                                myEvents={myEvents}
-                                setFavourite={setMyEvents}
-                                events={events}
-                                addMyEvent={addMyEvent}
-                                removeMyEvent={removeMyEvent}
-                                myFuckingEvents={events.filter(event =>
-                                    myEvents.includes(event.id)
-                                )}
-                            />
-                        )}
-                    />
-                    <Route
-                        path="/blablablabla"
-                        render={() => (
-                            <EventsCalendar
-                                events={events}
-                                userEvents={myEvents}
-                            />
-                        )}
-                    />
-                    <Redirect from="/home" to="/" />
-                </Switch>
-            </div>
-        </Router>
-    )
+  useEffect(() => {
+    console.log(myEvents);
+  }, [myEvents]);
+
+  const addMyEvent = id => {
+    setMyEvents([...myEvents, id]);
+  };
+
+  const removeMyEvent = id => {
+    const eventToDelete = myEvents.find(eventId => eventId === id);
+    const eventsWithDeletedEvent = myEvents.filter(
+      eventId => eventId !== eventToDelete
+    );
+    setMyEvents([...eventsWithDeletedEvent]);
+  };
+
+  return (
+    <Router>
+      <div>
+        <Navbar user={currentUser} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <div className="appView">
+                <MapView
+                  addMyEvent={addMyEvent}
+                  removeMyEvent={removeMyEvent}
+                  myEvents={myEvents}
+                />
+                <EventList
+                  myEvents={myEvents}
+                  setFavourite={setMyEvents}
+                  events={events}
+                  addMyEvent={addMyEvent}
+                  removeMyEvent={removeMyEvent}
+                />
+              </div>
+            )}
+          />
+          <Route path="/callender" component={App} />
+          <Route
+            path="/userProfile"
+            render={() => (
+              <UserProfile
+                myEvents={myEvents}
+                setFavourite={setMyEvents}
+                events={events}
+                addMyEvent={addMyEvent}
+                removeMyEvent={removeMyEvent}
+                myFuckingEvents={events.filter(event =>
+                  myEvents.includes(event.id)
+                )}
+              />
+            )}
+          />
+          <Route
+            path="/blablablabla"
+            render={() => (
+              <EventsCalendar events={events} userEvents={myEvents} />
+            )}
+          />
+          <Redirect from="/home" to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/GoButton.js
+++ src/components/GoButton.js
@@ -1,40 +1,46 @@
-import React, { Component } from "react"
-import Switch from "react-toggle-switch"
-import "../../node_modules/react-toggle-switch/dist/css/switch.min.css"
-import MaterialIcon from "material-icons-react"
+import React, { Component } from "react";
+import Switch from "react-toggle-switch";
+import "../../node_modules/react-toggle-switch/dist/css/switch.min.css";
+import MaterialIcon from "material-icons-react";
 
 export class GoButton extends Component {
-    state = {
-        switched: false
+  state = {
+    switched: false
+  };
+
+  componentDidMount() {
+    if (
+      Array.isArray(this.props.myEvents) &&
+      this.props.myEvents.includes(this.props.event.id)
+    ) {
+      this.setState({
+        switched: true
+      });
     }
 
-    componentDidMount() {
-        if (this.props.myEvents.includes(this.props.event.id)) {
-            this.setState({
-                switched: true
-            })
-        }
-    }
+    console.log(this.props);
+  }
 
-    toggleSwitch = () => {
-        this.setState(prevState => {
-            return { switched: !prevState.switched }
-        })
-        // debugger
-        this.state.switched
-            ? this.props.removeMyEvent(this.props.event.id)
-            : this.props.addMyEvent(this.props.event.id)
-    }
+  toggleSwitch = () => {
+    this.setState(prevState => {
+      return { switched: !prevState.switched };
+    });
+    // debugger
 
-    render() {
-        return (
-            <Switch
-                onClick={this.toggleSwitch}
-                on={this.state.switched}
-                className="switch"
-            >
-                {<MaterialIcon icon="directions_run" />}
-            </Switch>
-        )
-    }
+    this.state.switched
+      ? this.props.removeMyEvent(this.props.event.id)
+      : this.props.addMyEvent(this.props.event.id);
+  };
+
+  render() {
+    return (
+      <Switch
+        onClick={this.toggleSwitch}
+        on={this.state.switched}
+        className="switch"
+      >
+        {<MaterialIcon icon="directions_run" />}
+      </Switch>
+    );
+  }
 }
