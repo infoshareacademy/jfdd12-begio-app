b89028cffe99cb7983718254860c9cb8b5ba523e
lucasPA <lukasz.patalan@gmail.com>
2019-07-14 16:44:24 +0200

    fixes

--- src/components/UserProfile/UserMessage.js
+++ src/components/UserProfile/UserMessage.js
@@ -6,7 +6,7 @@ export class UserMessage extends Component {
         if (this.props.myEvents.length === 1) {
             return <p> <img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> event w Gdańsku. </p>
         } else if (this.props.myEvents.length > 1 && this.props.myEvents.length <= 4) {
-            return <p>Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> eventy w Gdańsku. </p>
+            return <p><img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> eventy w Gdańsku. </p>
         } else {
             return <p> <img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> eventów w Gdańsku. </p>
         }
