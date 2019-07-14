af0e5ed0b3360fb7d38965649c7ca9b46b629b10
lucasPA <lukasz.patalan@gmail.com>
2019-07-14 17:12:38 +0200

    imges bugs fixed

--- src/components/UserProfile/UserMessage.js
+++ src/components/UserProfile/UserMessage.js
@@ -4,14 +4,14 @@ export class UserMessage extends Component {
     render() {
 
         if (this.props.myEvents.length === 1) {
-            return <p> <img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzenie. </p>
+            return <p> <img className="infoImg" src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzenie. </p>
         } else if (this.props.myEvents.length > 1 && this.props.myEvents.length <= 4) {
-            return <p><img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzenia. </p>
+            return <p><img className="infoImg" src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzenia. </p>
         } else if (this.props.myEvents.length === 0)
-            return <p> <img src={info} /> Witaj <b>{this.props.userName}</b>, nie jesteś jeszcze zapisany na żadne wydarzenie. </p>
+            return <p> <img className="infoImg" src={info} /> Witaj <b>{this.props.userName}</b>, nie jesteś jeszcze zapisany na żadne wydarzenie. </p>
 
         else {
-            return <p> <img src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzeń. </p>
+            return <p> <img className="infoImg" src={info} /> Witaj <b>{this.props.userName}</b>, jesteś zapisany na <b>{this.props.myEvents.length}</b> wydarzeń. </p>
         }
     }
 }
