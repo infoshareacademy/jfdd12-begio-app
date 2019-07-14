032c23f74adf27033e21d34393d01f50b59c8b93
TekMal <k.kochanowska6@gmail.com>
2019-07-14 15:36:31 +0200

    add styles into tooltip

--- src/components/InfoWindowView.js
+++ src/components/InfoWindowView.js
@@ -3,6 +3,7 @@ import { EventName } from "./EventName";
 import { EventPlace } from "./EventPlace";
 import { EventDate } from "./EventDate";
 import { GoButton } from "./GoButton";
+import './InfoWindowView.css'
 export function InfoWindowView({
   myEvents,
   addMyEvent,
@@ -11,54 +12,35 @@ export function InfoWindowView({
   toggleEventDetails
 }) {
   return (
-    <>
-      <div
-        style={{
-          margin: "0",
-          padding: "0",
-          width: "120px",
-          height: "140px",
-          fontSize: "10px"
-        }}
-      >
-        <img
-          onClick={toggleEventDetails}
-          alt="sd"
-          style={{
-            cursor: "pointer",
-            display: "block",
-            marginLeft: "auto",
-            marginRight: "auto",
-            width: "76%"
-          }}
-          src={event.images[0]}
-        />
-        <p>
-          <p
-            style={{
-              fontWeight: "bold",
-              color: "rgb(68, 66, 105)",
-              cursor: "pointer",
-              margin: "0"
-            }}
-            onClick={toggleEventDetails}
-          >
-            <EventName event={event} />
-          </p>
-
-          <EventPlace event={event} />
-          <br />
-          <b>
-            <EventDate event={event} />
-          </b>
-        </p>
-        <GoButton
-          addMyEvent={addMyEvent}
-          removeMyEvent={removeMyEvent}
-          event={event}
-          myEvents={myEvents}
-        />
-      </div>
-    </>
+    <div className="infoWindowCointainer">
+          <div className="infoWindowEventInfo">
+            <img
+              className="infoWindowImage"
+              onClick={toggleEventDetails}
+              alt="sd"
+              src={event.images[0]}
+            />
+            <p>
+              <h1 className="infoWindowEventName"
+                   onClick={toggleEventDetails} >
+                  <EventName event={event} />
+              </h1>
+              <p>
+                  <EventPlace event={event} />
+              </p>
+              <p>
+                  <EventDate event={event} />
+              </p>
+            </p>
+          </div>
+          <div className="infoWindowGoButton">
+            <GoButton
+              addMyEvent={addMyEvent}
+              removeMyEvent={removeMyEvent}
+              event={event}
+              myEvents={myEvents}
+            />
+          </div>
+  </div>    
   );
 }
