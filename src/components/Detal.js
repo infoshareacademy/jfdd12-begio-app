b524a187e30f53cc512ea0e5ca5396be7de4ba16
TekMal <k.kochanowska6@gmail.com>
2019-07-14 16:38:25 +0200

    wip detail view

--- src/components/Detal.js
+++ src/components/Detal.js
@@ -8,7 +8,11 @@ import { EventDescription } from "./EventDescription"
 export function Detal({ event }) {
     return (
         <div className="detalContainer">
-            <center><h3 style={{ marginBottom: "0" }}>{event.title} <button style={{ marginBottom: "5px" }}>GO!</button></h3></center>
+            <center>
+                <h3 style={{ marginBottom: "0" }}>{event.title} 
+                <button style={{ marginBottom: "5px" }}>GO!</button>
+                </h3>
+                </center>
             <p><EventPlace event={event} /></p>
             <p><img style={{ width: "3%", marginRight: "5px" }} alt="cal" src={calendar} />
                 <EventDate event={event} />
@@ -19,16 +23,3 @@ export function Detal({ event }) {
         </div>
     );
 }
-
-
-// title={this.state.selectedEvent.title}
-//               city={this.state.selectedEvent.address.city}
-//               street={this.state.selectedEvent.address.street}
-//               houseNumber={this.state.selectedEvent.address.houseNumber}
-//               day={this.state.selectedEvent.startDate.day}
-//               month={this.state.selectedEvent.startDate.month}
-//               year={this.state.selectedEvent.startDate.year}
-//               photoOne={this.state.selectedEvent.images[0]}
-//               photoTwo={this.state.selectedEvent.images[1]}
-//               photoThree={this.state.selectedEvent.images[2]}
-//               description={this.state.selectedEvent.description} 
