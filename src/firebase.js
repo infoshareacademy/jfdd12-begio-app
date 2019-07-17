71886df47202997a22799cabbbb0f5e12ff1bf80
Kamila Kochanowska <k.kochanowska6@gmail.com>
2019-07-17 16:49:17 +0200

    add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCk4uXi8WwSHqWFc_SvfdEjyn3CBsqyiMw",
+    authDomain: "gogdansk-begio-app.firebaseapp.com",
+    databaseURL: "https://gogdansk-begio-app.firebaseio.com",
+    projectId: "gogdansk-begio-app",
+    storageBucket: "",
+    messagingSenderId: "919816780208",
+    appId: "1:919816780208:web:510def1b7a38baf0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
