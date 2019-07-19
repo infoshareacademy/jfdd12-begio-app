e46d1ba8ff48a2b40e904f3021a6645fbbaf6dff
Andrzej Zaborski <andrzejzab@gmail.com>
2019-07-19 17:13:23 +0200

    WIP

--- src/services/UsersEventService.js
+++ src/services/UsersEventService.js
@@ -13,15 +13,15 @@ export const fetchMyEvents = () => {
 export const addEvents = eventId => {
     const uid = "28b919ba-7570-477a-9fe1-67d7729d5bb5"
     const userEventsReference = `users/${uid}/events`
-    let userEvents = fetchMyEvents()
-    //   const userId = firebase.auth().currentUser.uid;
+    fetchMyEvents().then(events => {
+        const eventsUpdate =
+            events instanceof Array ? [...events, eventId] : [eventId]
+        // const eventsUpdate = [1, 2]
 
-    const eventsUpdate =
-        userEvents instanceof Array ? [...userEvents, eventId] : [eventId]
-    // const eventsUpdate = [1, 2]
-
-    firebase
-        .database()
-        .ref(userEventsReference)
-        .set(eventsUpdate)
+        firebase
+            .database()
+            .ref(userEventsReference)
+            .set(eventsUpdate)
+    })
+    //   const userId = firebase.auth().currentUser.uid
 }
