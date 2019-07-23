8ea7592c9b6b0084dbe96a6bd736f8856b7e824d
Ania Jelinska <ania.jelin@wp.pl>
2019-07-23 16:01:44 +0200

    add style to PhotoGallery

--- src/components/Carusel.js
+++ src/components/Carusel.js
@@ -4,31 +4,27 @@ import "react-responsive-carousel/lib/styles/carousel.css";
 import "react-responsive-carousel/lib/styles/main.css";
 import "react-responsive-carousel/lib/styles/main.min.css";
 import { Carousel } from "react-responsive-carousel";
+import styles from "./PhotoGallery.module.css";
 
 export function PhotoGallery(props) {
   return (
-      <div
-        style={{
-          width: "700px",
-          margin: "0 auto"
-        }}
+    <div className={styles.photo}>
+      <Carousel
+        showStatus={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={3000}
       >
-        <Carousel
-          showStatus={false}
-          infiniteLoop={true}
-          autoPlay={true}
-          interval={3000}
-        >
-          <div>
-            <img alt="d" src={props.one} />
-          </div>
-          <div>
-            <img alt="sd" src={props.two} />
-          </div>
-          <div>
-            <img alt="sd" src={props.three} />
-          </div>
-        </Carousel>
-      </div>
+        <div>
+          <img alt="d" src={props.one} />
+        </div>
+        <div>
+          <img alt="sd" src={props.two} />
+        </div>
+        <div>
+          <img alt="sd" src={props.three} />
+        </div>
+      </Carousel>
+    </div>
   );
 }
