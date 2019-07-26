4b436b6569a6761f8d8b09c19f078a5dda46e2f8
Andrzej Zaborski <andrzejzab@gmail.com>
2019-07-26 16:54:24 +0200

    modal popup validate nesting warning removed

--- src/components/EventDescription.js
+++ src/components/EventDescription.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from "react"
 
 export const EventDescription = ({ event }) => {
-  return (
-    <p
-      style={{
-        border: "1px solid grey",
-        borderRadius: "10px",
-        padding: "15px"
-      }}
-    >
-      {event.description}
-    </p>
-  );
-};
+    return (
+        <p
+            style={{
+                border: "1px solid grey",
+                borderRadius: "10px",
+                padding: "15px"
+            }}
+        >
+            {event.description}
+        </p>
+    )
+}
