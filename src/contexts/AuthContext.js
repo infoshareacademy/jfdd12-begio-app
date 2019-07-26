2fd15cc0a12815c82ceb8f2e7decd6afb8303555
Andrzej Zaborski <andrzejzab@gmail.com>
2019-07-26 16:13:49 +0200

    firebase warning dealt with

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, createContext } from "react"
-import firebase from "firebase"
+import firebase from "firebase/app"
+import "firebase/auth"
 
 export const AuthContext = createContext()
 
